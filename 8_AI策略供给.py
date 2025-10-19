9a4d5f5840891928da2cd220bd4f1009b7373be5
SingerNo01 <729470867@qq.com>
2025-10-19 15:47:39 +0800

    Add files via upload

--- 8_AI策略供给.py
+++ 8_AI策略供给.py
@@ -15,4 +15,4 @@ st.components.v1.html("""
     <h2>正在打开智能农业助手...</h2>
     <p>如果浏览器阻止了弹出窗口，请允许该网站的弹出窗口或<a href="https://udify.app/chatbot/6MCrcJyUrhQwiY5P" target="_blank">点击这里手动打开</a></p>
 </div>
-""", height=400)
+""", height=400)sadfsdafadf
