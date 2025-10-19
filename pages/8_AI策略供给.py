fcd78b524363ddbbebb549cfb9e5b1d974c9d003
SingerNo01 <729470867@qq.com>
2025-10-19 16:32:09 +0800

    Add files via upload

--- pages/8_AI策略供给.py
+++ pages/8_AI策略供给.py
@@ -1,39 +1,26 @@
 import streamlit as st
 
-st.set_page_config(
-    page_title="智能农业助手",
-    layout="wide",
-    initial_sidebar_state="collapsed"
+st.set_page_config(layout="wide")
+
+st.title("测试页面")
+
+# 方法1：直接iframe
+st.markdown("### 方法1：直接iframe")
+st.components.v1.iframe(
+    "http://119.45.173.154/chatbot/Rr8QS2s9GIvD9ndV",
+    height=600,
+    scrolling=True
 )
 
-# 隐藏Streamlit默认元素
+# 方法2：HTML方式
+st.markdown("### 方法2：HTML方式")
 st.markdown("""
-<style>
-    #MainMenu {visibility: hidden;}
-    footer {visibility: hidden;}
-    header {visibility: hidden;}
-    .stApp {
-        padding: 0;
-        margin: 0;
-    }
-    .block-container {
-        padding-top: 0;
-        padding-bottom: 0;
-        padding-left: 0;
-        padding-right: 0;
-        max-width: 100%;
-    }
-</style>
-""", unsafe_allow_html=True)
-
-# 嵌入您的聊天机器人
-st.markdown("""
-<div style="width: 100%; height: 100vh;">
-    <iframe
-        src="http://119.45.173.154/chatbot/Rr8QS2s9GIvD9ndV"
-        style="width: 100%; height: 100vh; border: none;"
-        frameborder="0"
-        allow="microphone">
-    </iframe>
+<div style="border: 2px solid red; height: 600px;">
+    <iframe src="http://119.45.173.154/chatbot/Rr8QS2s9GIvD9ndV" 
+            style="width:100%; height:100%; border:none;"></iframe>
 </div>
 """, unsafe_allow_html=True)
+
+# 方法3：直接链接
+st.markdown("### 方法3：直接访问")
+st.markdown('[点击这里直接访问聊天机器人](http://119.45.173.154/chatbot/Rr8QS2s9GIvD9ndV)')
