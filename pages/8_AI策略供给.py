17cdc03e4110906f9cfd3802d30f1d4b6112ce1f
SingerNo01 <729470867@qq.com>
2025-10-19 17:23:55 +0800

    Add files via upload

--- pages/8_AI策略供给.py
+++ pages/8_AI策略供给.py
@@ -1,4 +1,9 @@
 import streamlit as st
+import os
+
+# 禁用安全限制以允许 HTTP 内容
+os.environ['STREAMLIT_SERVER_ENABLE_XSRF'] = 'false'
+os.environ['STREAMLIT_SERVER_ENABLE_CORS'] = 'false'
 
 st.set_page_config(
     page_title="智能农业助手",
@@ -6,7 +11,7 @@ st.set_page_config(
     initial_sidebar_state="collapsed"
 )
 
-# 隐藏Streamlit默认元素
+# 隐藏Streamlit默认元素并添加允许 HTTP 的样式
 st.markdown("""
 <style>
     #MainMenu {visibility: hidden;}
@@ -23,17 +28,22 @@ st.markdown("""
         padding-right: 0;
         max-width: 100%;
     }
+    /* 允许混合内容 */
+    iframe {
+        -webkit-security-policy: mixed-content;
+    }
 </style>
 """, unsafe_allow_html=True)
 
-# 使用HTTPS网址嵌入您的聊天机器人
+# 嵌入您的聊天机器人
 st.markdown("""
 <div style="width: 100%; height: 100vh;">
     <iframe
-        src="https://7d1f6f09ce18c6ac8a42c78ad372e9a0.serveo.net/chatbot/Rr8QS2s9GIvD9ndV"
+        src="http://119.45.173.154/chatbot/Rr8QS2s9GIvD9ndV"
         style="width: 100%; height: 100vh; border: none;"
         frameborder="0"
-        allow="microphone">
+        allow="microphone *"
+        allowfullscreen>
     </iframe>
 </div>
 """, unsafe_allow_html=True)
